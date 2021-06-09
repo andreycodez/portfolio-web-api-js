ec227003a151699c586f0f61ff3a67265eac9b46
Andrey Codez <andreycodez@bixtrix.com>
2021-06-09 17:26:40 +1000

    Getting data from the random user

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+// Start API queue
+
+const usersCount = 30;
+const listUsers = [];
+let didUsersLoad = false
+let isTimer = false;
+
+listUsers.push('John')
+
+const getUser = (number) => {
+    for (let i = 0; i < number; i++) {
+        const randomUser = fetch('https://random-data-api.com/api/users/random_user');
+        randomUser
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+                listUsers.push(data)
+            })
+            .catch((error) => console.log(error));
+    }
+}
+
+console.log(listUsers)
+
+function generateUsersList(data) {
+    const parentHolder = document.getElementById('usersListId');
+    for (const man of data) {
+        const nameHolder = document.createElement('p');
+        nameHolder.innerHTML = man[0];
+        const holder = document.createElement('div');
+        holder.setAttribute('class', 'user');
+        holder.appendChild(nameHolder);
+        parentHolder.appendChild(holder);
+    }
+
+}
+
+getUser(usersCount);
+
+generateUsersList(listUsers);
+
+
