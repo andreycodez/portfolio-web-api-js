e19086116c3980427d1d85c1c4c036657ad57925
Andrey Codez <andreycodez@bixtrix.com>
2021-06-09 18:54:22 +1000

    Showing data from API finally

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,37 @@
-// Start API queue
+// Getting Random Users from API
 
-const usersCount = 30;
-const listUsers = [];
+const usersCount = 30
+let dataFromAPI = []
 let didUsersLoad = false
-let isTimer = false;
+let isTimer = false
 
-listUsers.push('John')
-
-const getUser = (number) => {
-    for (let i = 0; i < number; i++) {
-        const randomUser = fetch('https://random-data-api.com/api/users/random_user');
+const p1 = new Promise( (resolve, reject) => {
+    console.log('Started the API')
+    const listUsers = []
+    for (let i = 0; i < 30; i++) {
+        const randomUser = fetch('https://random-data-api.com/api/users/random_user')
         randomUser
             .then((response) => response.json())
             .then((data) => {
-                console.log(data)
+                //console.log(data)
                 listUsers.push(data)
             })
-            .catch((error) => console.log(error));
+            .catch((error) => console.log(error))
     }
-}
+    setTimeout(() => {
+        resolve(listUsers)
+    }, 10000)
+}).then((data) => {
+    dataFromAPI = data
+    generateUsersList(dataFromAPI)
 
-console.log(listUsers)
+})
 
 function generateUsersList(data) {
     const parentHolder = document.getElementById('userListId');
-    console.log(parentHolder);
     for (const man of data) {
         const nameHolder = document.createElement('p');
-        nameHolder.innerHTML = man;
+        nameHolder.innerHTML = man.first_name;
         const holder = document.createElement('div');
         holder.setAttribute('class', 'user');
         holder.appendChild(nameHolder);
@@ -35,9 +39,3 @@ function generateUsersList(data) {
     }
 
 }
-
-getUser(usersCount);
-
-generateUsersList(listUsers);
-
-
